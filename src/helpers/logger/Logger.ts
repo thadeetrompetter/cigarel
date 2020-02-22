d56e60f4a35ba7b66b6ce9549860bf875e225037
Thadee Trompetter <thadee@hotmail.com>
2020-02-22 22:02:27 +0100

    change log format

--- src/helpers/logger/Logger.ts
+++ src/helpers/logger/Logger.ts
@@ -1,4 +1,4 @@
-import { createLogger, transports, Logger } from "winston"
+import { createLogger, transports, format, Logger } from "winston"
 
 export type ILogger = Logger
 export type LogLevel = "error"
@@ -8,7 +8,15 @@ export type LogLevel = "error"
   | "debug"
   | "silly"
 
+const { combine, timestamp, printf } = format
+
 export const getLogger = (level: LogLevel): ILogger => createLogger({
+  format: combine(
+    timestamp({
+      format: "YYYY-MM-DD HH:mm:ss"
+    }),
+    printf(({ timestamp, message, level }) => `${timestamp} | ${level}: ${message}`)
+  ),
   transports: [
     new transports.Console({ level })
   ]
